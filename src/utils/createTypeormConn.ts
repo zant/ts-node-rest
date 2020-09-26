5cd4d14ae33225e3a2ac15b597b90ebc653ba78d
gonza <gonzalo.christobal@gmail.com>
2020-09-26 17:06:33 -0400

    Better typing

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -1,10 +1,11 @@
 import {
+  Connection,
   createConnection,
   getConnectionManager,
   getConnectionOptions,
 } from "typeorm";
 
-export const createTypeormConn = async () => {
+export const createTypeormConn = async (): Promise<Connection | undefined> => {
   if (getConnectionManager().has(process.env.NODE_ENV as string)) return;
 
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
