346cd066c0f0d0814399b3105ccfd6e90985b79c
gonza <gonzalo.christobal@gmail.com>
2020-09-26 17:07:49 -0400

    Add random route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { acronym } from "./acronym";
+import { random } from "./random";
 
 export const router = Router();
 
 router.use("/acronym", acronym);
+router.use("/random", random);
