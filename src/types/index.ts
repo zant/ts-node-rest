c1c145d8f2ce0b3380cc1b8dc54343196670a1fd
gonza <gonzalo.christobal@gmail.com>
2020-09-30 17:17:59 -0400

    Add types

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,14 @@ export interface AcronymObject {
 export type FormattedError = { path: string; message: string };
 
 export type Maybe<T> = T | undefined;
+
+export interface ServerError {
+  error?: Error;
+  status?: number;
+  errors: ClientError[];
+}
+
+export interface ClientError {
+  message?: string;
+  path?: string;
+}
