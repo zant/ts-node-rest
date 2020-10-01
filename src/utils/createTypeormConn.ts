ca4f99d35977e4b47cc614dbfd008092624e73b8
gonza <gonzalo.christobal@gmail.com>
2020-10-01 00:10:59 -0400

    Add spaces

--- src/utils/createTypeormConn.ts
+++ src/utils/createTypeormConn.ts
@@ -8,10 +8,12 @@ import {
 export const createTypeormConn = async (): Promise<Connection | undefined> => {
   if (getConnectionManager().has("default")) return;
   let dbName = process.env.NODE_ENV;
+  
   const isDocker = process.env.__DOCKER__;
   if (isDocker) {
     dbName = `${dbName}-container`;
   }
+
   const connectionOptions = await getConnectionOptions(dbName);
   return createConnection({ ...connectionOptions, name: "default" });
 };
